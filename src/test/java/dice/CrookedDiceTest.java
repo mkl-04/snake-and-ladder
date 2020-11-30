b4df67055165f9a50a5232a6edb0ff55d869724b
mkl <mohit.laad4@gmail.com>
2020-11-30 17:27:35 +0530

    added implementation for CrookedDiceTest

--- src/test/java/dice/CrookedDiceTest.java
+++ src/test/java/dice/CrookedDiceTest.java
@@ -1,6 +1,8 @@
 package dice;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +15,20 @@ public class CrookedDiceTest {
     @Before
     public void setup() {
         dice = new CrookedDice();
-        diceArray = Arrays.asList( 2, 4, 6);
+        diceArray = Arrays.asList(2, 4, 6);
+    }
+
+    @Test
+    public void testThrowDice() {
+        Integer number = dice.throwDice();
+        Assert.assertTrue("dice should have a number between 2,4, 6", diceArray.contains(number));
+    }
+
+    @Test
+    public void testThrowDiceMultipleTimes() {
+        for (int i = 0; i < 10; i++) {
+            Integer number = dice.throwDice();
+            Assert.assertTrue("dice should have a number between 2,4, 6", diceArray.contains(number));
+        }
     }
 }
